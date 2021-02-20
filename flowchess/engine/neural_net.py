e450220ff1f69bb29d59c967845b3f206adc0261
Nick Zullo <njz12@pitt.edu>
2021-02-20 16:29:12 -0500

    Initial Neural Net

--- flowchess/engine/neural_net.py
+++ flowchess/engine/neural_net.py
@@ -1,23 +1,34 @@
 import torch
 from torch import nn
-import mcts
 import math
 
 """
-Given a state of the board
+Given a state of the board, return the probability it is a good position
 """
 
 class NeuralNet(nn.Module):
-	def __init__(self):
+	def __init__(self, lr=.01, device="cpu"):
 		super().__init__()
 
-		self.input = nn.Linear(64,32)
-		self.hidden = nn.Linear(32,20)
-		self.output = nn.Linear(20,1)
+		self.device = device
+		self.input = nn.Linear(64,20,bias=True)
+		self.hidden = nn.Linear(20,20,bias=True)
+		self.output = nn.Linear(20,1,bias=True)
+		self.optim = torch.optim.Adam(self.parameters(), lr=lr)
+		self.to(device)
 
 	def forward(self, x):
-		x = nn.functional.relu(self.input(x))		#ReLU activation
-		x = nn.functional.relu(self.hidden(x))
-		x = torch.sigmoid(self.output(x))			#Sigmoid activation
+		x = nn.functional.leaky_relu(self.input(x))		#ReLU activation
+		x = nn.functional.leaky_relu(self.hidden(x))
+		x = torch.tanh(self.output(x))			#Sigmoid activation
+		return x
 
-
+	def backward(self, y_pred, y):
+		loss_fn = nn.MSELoss()
+		boxed_y_pred = torch.tensor(y_pred).to(self.device)
+		boxed_y = torch.tensor(y).to(self.device)
+		loss = loss_fn(boxed_y_pred, boxed_y)
+		loss.requires_grad = True
+		self.optim.zero_grad()
+		loss.backward()
+		self.optim.step()
--- flowchess/main.py
+++ flowchess/main.py
@@ -1,10 +1,10 @@
 import chess
 import engine.mcts as mcts
+import engine.neural_net as net
 
 board = chess.Board()
+nn = net.NeuralNet(lr=.01,device="cuda:0")
 
-game = mcts.MCTS(board)
-game.run(iters=5000)
-
-print(f"{game.white_wins} {game.black_wins}")
+game = mcts.MCTS(board, net=nn)
+game.run(iters=100)
 print(game.last_win.move_stack)
